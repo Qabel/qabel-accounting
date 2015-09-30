d2eb827425642f1f1a862b6ad6ee28faf1f04b46
Jens Kadenbach <gitdax@daxbau.net>
2015-09-30 16:22:44 +0200

    Remove quota test
    The quota test will always fail on sqlite

--- qabel_provider/test_models.py
+++ qabel_provider/test_models.py
@@ -1,21 +1,8 @@
-import pytest
-
-
 def test_user(user):
     """ Check that we have a profile
     """
     assert user.profile.quota == 0
     assert not user.is_staff
-
-
-@pytest.fail
-def test_positive_quota(user):
-    """
-    Allowed to fail because sqlite does not enforce integer constraints
-    """
-    user.profile.quota = -1
-    user.profile.save()
-    assert user.profile.quota == 0
 
 
 def test_superuser(admin_user):
