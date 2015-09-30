c754769f668b9341ac97f9f898ae123fa81edc94
Jens Kadenbach <gitdax@daxbau.net>
2015-09-30 16:15:20 +0200

    User profile page which shows the quota
    * Account stuff moved to /accounts
    * Profile page at /accounts/profile

--- functional_tests/test_accounts.py
+++ functional_tests/test_accounts.py
@@ -16,12 +16,19 @@ def test_admin_login(base_url, admin_user, browser):
     assert 'Storage quota:' in browser.html
 
 
+def test_login_required(base_url, browser):
+    browser.visit('{0}/accounts/profile/'.format(base_url))
+    assert browser.find_by_id('id_username')
+
+
 def test_login(base_url, user, browser):
-    browser.visit('{0}/login/'.format(base_url))
+    browser.visit('{0}/accounts/login/'.format(base_url))
     browser.fill_form({'username': 'qabel_user',
                        'password': 'password'})
     browser.find_by_id('id_submit').click()
-    # Since we don't have a profile page, it _should_ actually 404.
-    assert browser.status_code == 404
+
+    assert 'Storage quota: {0}'.format(user.profile.quota)\
+           == browser.find_by_id('id_quota').first.text
+
 
 
--- qabel_id/urls.py
+++ qabel_id/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from qabel_provider import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url('^', include('django.contrib.auth.urls'))
+    url('^accounts/', include('django.contrib.auth.urls')),
+    url('^accounts/profile', views.profile),
 ]
