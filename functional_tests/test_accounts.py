5cf8f37a820cdf262f61691f51718acb5213cbee
Jens Kadenbach <gitdax@daxbau.net>
2015-09-30 15:58:23 +0200

    Integrate user auth
    * Functionl tests with splinter and phantomjs
    * Integration of profiles into admin interface
    * Migration to create tables for profiles

---
+++ functional_tests/test_accounts.py
@@ -0,0 +1,27 @@
+import pytest
+
+
+@pytest.fixture
+def base_url(live_server):
+    return live_server.url
+
+
+def test_admin_login(base_url, admin_user, browser):
+    browser.visit('{0}/admin/'.format(base_url))
+    browser.fill_form({'username': 'admin',
+                       'password': 'password'})
+    browser.find_by_value('Log in').click()
+    browser.click_link_by_href('/admin/auth/user/')
+    browser.click_link_by_text('admin')
+    assert 'Storage quota:' in browser.html
+
+
+def test_login(base_url, user, browser):
+    browser.visit('{0}/login/'.format(base_url))
+    browser.fill_form({'username': 'qabel_user',
+                       'password': 'password'})
+    browser.find_by_id('id_submit').click()
+    # Since we don't have a profile page, it _should_ actually 404.
+    assert browser.status_code == 404
+
+
--- qabel_provider/test_admin.py
+++ qabel_provider/test_admin.py
@@ -3,7 +3,3 @@ def test_an_admin_view(admin_client):
     response = admin_client.get('/admin/')
     assert response.status_code == 200
 
-
-def test_inline_user_profile(admin_client):
-    response = admin_client.get('/admin/auth/user/1/')
-    assert b'Storage quota:' in response.content
