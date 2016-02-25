2fe2a8a1905bf4d3b423ab6e609bd4ea0fb5d86f
Jens Kadenbach <gitdax@daxbau.net>
2016-02-25 15:53:51 +0100

    Logging config as json file
    The logging config can be set in a json file. The path of that config
    file is set in LOGGING_CONFIG_FILE

---
+++ qabel_id/settings/local_settings.example.py
@@ -0,0 +1,8 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+API_SECRET = 'Changeme'
+
+STATIC_URL = '/static/'
+
+
--- qabel_id/settings/production_settings.py
+++ qabel_id/settings/production_settings.py
@@ -1,4 +1,6 @@
 from .base_settings import *
+import json
+ALLOWED_HOSTS = ['localhost']
 
 DEBUG = False
 
@@ -20,13 +22,18 @@ DATABASES = {
     }
 }
 
+LOGGING_CONFIG_FILE = os.path.join(BASE_DIR, '..', 'logging.json')
+
 try:
     from .local_settings import *
 except ImportError:
     import sys
+
     print("""\
 To use the production settings, create a file 'local_settings.py' in qabel_id/settings
  which defines at least the options SECRET_KEY, API_SECRET and DATABASES\
 """, file=sys.stderr)
     exit(1)
 
+with open(LOGGING_CONFIG_FILE) as config_file:
+    LOGGING = json.load(config_file)
