6221f9f754c8d0bf1b16edf7fbead1d584d27bd5
Jens Kadenbach <gitdax@daxbau.net>
2016-03-10 15:51:21 +0100

    Working local_settings

--- qabel_id/settings/local_settings.example.py
+++ qabel_id/settings/local_settings.example.py
@@ -12,13 +12,10 @@ CACHES = {
     'default': {
         'BACKEND': 'redis_cache.RedisCache',
         'LOCATION': [
-            '<host>:<port>',
-            '<host>:<port>',
-            '<host>:<port>',
+            'localhost:6379',
         ],
         'OPTIONS': {
-            'DB': 2,
-            'PASSWORD': 'yadayada',
+            'DB': 1,
             'PARSER_CLASS': 'redis.connection.HiredisParser',
             'CONNECTION_POOL_CLASS': 'redis.BlockingConnectionPool',
             'CONNECTION_POOL_CLASS_KWARGS': {
