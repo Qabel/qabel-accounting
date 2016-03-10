684e182d08735abb6ff49a21dc2d799d15819ab5
Jens Kadenbach <gitdax@daxbau.net>
2016-03-10 13:51:23 +0100

    Include redis caching for login throttle

--- qabel_id/settings/local_settings.example.py
+++ qabel_id/settings/local_settings.example.py
@@ -8,3 +8,25 @@ STATIC_URL = '/static/'
 CORS_ORIGIN_WHITELIST = (
     #'qabel.dev',
 )
+CACHES = {
+    'default': {
+        'BACKEND': 'redis_cache.RedisCache',
+        'LOCATION': [
+            '<host>:<port>',
+            '<host>:<port>',
+            '<host>:<port>',
+        ],
+        'OPTIONS': {
+            'DB': 2,
+            'PASSWORD': 'yadayada',
+            'PARSER_CLASS': 'redis.connection.HiredisParser',
+            'CONNECTION_POOL_CLASS': 'redis.BlockingConnectionPool',
+            'CONNECTION_POOL_CLASS_KWARGS': {
+                'max_connections': 50,
+                'timeout': 20,
+            },
+            'MAX_CONNECTIONS': 1000,
+            'PICKLE_VERSION': -1,
+        },
+    },
+}
