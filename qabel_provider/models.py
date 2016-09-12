7ac47a0167c7a2ffa416c7901bbd5807f371f421
Marian Beermann <beermann@qabel.de>
2016-09-12 11:59:41 +0200

    PlanInterval.start: wrap audit log and interval update in DB transaction

--- qabel_provider/models.py
+++ qabel_provider/models.py
@@ -127,10 +127,11 @@ class PlanInterval(models.Model, ExportModelOperationsMixin('planinterval')):
             raise ValueError('Cannot start using a %s interval' % self.state)
         self.state = 'in_use'
         self.started_at = datetime.datetime.now()
-        self.save()
         audit_log = ProfilePlanLog(profile=self.profile,
                                    action='start-interval', plan=self.plan, interval=self)
-        audit_log.save()
+        with transaction.atomic():
+            self.save()
+            audit_log.save()
 
     @classmethod
     def get_or_start_interval(model, profile):
