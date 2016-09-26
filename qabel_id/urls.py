2a30466dfb1da0c7c7a2658d2ae8aabac110330d
Marian Beermann <beermann@qabel.de>
2016-09-26 19:35:30 +0200

    !FACET_USER_PROFILE: define /

--- qabel_id/urls.py
+++ qabel_id/urls.py
@@ -70,7 +70,9 @@ profile_urls = [
 if not settings.FACET_USER_PROFILE:
     # knock knock
     settings.MENU = []
-    profile_urls.clear()
+    profile_urls = [
+        url(r'^$', lambda request: redirect('https://www.qabel.de/'), name='user-profile'),
+    ]
 
 
 dispatch_urls = [
