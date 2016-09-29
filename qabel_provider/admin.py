3403a988ecd4cdc317d09b080150ba78ff531b83
Marian Beermann <beermann@qabel.de>
2016-09-29 14:21:15 +0200

    Add export user data action to admin

--- qabel_provider/admin.py
+++ qabel_provider/admin.py
@@ -1,6 +1,10 @@
+import csv
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as OriginalUserAdmin
 from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
 import nested_admin
@@ -52,6 +56,30 @@ class UserAdmin(OriginalUserAdmin, nested_admin.NestedModelAdmin):
     inlines = [UserProfileInline]
     list_filter = OriginalUserAdmin.list_filter + \
         ('profile__plus_notification_mail', 'profile__pro_notification_mail')
+
+    actions = ('export_user_data',)
+
+    def export_user_data(self, request, queryset):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=Qabel-User-Data-%s.csv' % timezone.now().replace(microsecond=0).isoformat()
+
+        csv_writer = csv.writer(response)
+        csv_writer.writerow(['username', 'email'])
+        failed_no_address = 0
+        written_rows = 0
+        for user in queryset:
+            email = user.profile.primary_email
+            if not email or not email.verified:
+                failed_no_address += 1
+                continue
+            csv_writer.writerow([user.username, email.email])
+            written_rows += 1
+        self.message_user(request, _('admin user data export {written_rows} {failed_no_address}').format(
+            written_rows=written_rows,
+            failed_no_address=failed_no_address,
+        ))
+        return response
+    export_user_data.short_description = _('admin user action export data label')
 
 
 class PlanAdmin(admin.ModelAdmin):
