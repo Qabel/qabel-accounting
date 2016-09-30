b8c588709217859a0ce592c1170df5052f43f6fb
Marian Beermann <beermann@qabel.de>
2016-09-30 11:51:05 +0200

    CSV export test: cover selection criteria

--- qabel_provider/test_admin.py
+++ qabel_provider/test_admin.py
@@ -1,3 +1,5 @@
+from allauth.account.models import EmailAddress
+from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
 
@@ -12,10 +14,25 @@ def test_export_user_data(admin_client, user):
     primary_email = user.profile.primary_email
     primary_email.verified = True
     primary_email.save()
+
+    # No email address
+    no_mail = User.objects.create_user('no_mail', 'no_mail@example.com', 'password')
+
+    # No confirmed email address
+    unconfirmed = User.objects.create_user('unconfirmed', 'unconfirmed@example.com', 'password')
+    EmailAddress.objects.create(user=unconfirmed, email=unconfirmed.email, primary=True)
+
+    # Unrelated user
+    User.objects.create_user('unrelated', 'unrelated@example.com', 'password')
+
     response = admin_client.post(change_url, {
         'action': 'export_user_data',
-        '_selected_action': [user.pk],
+        '_selected_action': [user.pk, no_mail.pk, unconfirmed.pk],
     })
     assert response.status_code == 200
     assert response['Content-Type'] == 'text/csv'
-    assert 'qabel_user,qabeluser@example.com' in response.content.decode()
+    content = response.content.decode()
+    assert 'qabel_user,qabeluser@example.com' in content
+    assert 'no_mail' not in content
+    assert 'unconfirmed' not in content
+    assert 'unrelated' not in content
